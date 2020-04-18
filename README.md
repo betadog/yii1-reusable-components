f3498c7b11c554135db937e213b4d6f63dba5713
kozitsyn <kozitsyn@kassir.ru>
2020-04-18 19:18:04 +0300

    MiniTrace

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # yii1-reusable-components
+
+## MiniTrace
+
+This behavior allow u get trace in any point of code: `echo Yii::app()->miniTrace();` 
+
+Just add follow code into `./protected/config/main.php`
+
+```
+'behaviors'         => [
+    'miniTrace' => [
+        'class' => 'application.components.behaviors.MiniTrace',
+    ],
+],
+```
+
+
---
+++ yii1/behaviors/MiniTrace.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Class MiniTrace
+ *
+ * Usage:
+ * Add those to ./protected/config/main.php
+<pre>
+    'behaviors'         => [
+        'miniTrace' => [
+            'class' => 'application.components.behaviors.MiniTrace',
+        ],
+    ],
+</pre>
+ *
+ * And now u can use it anywhere like:
+ * echo Yii:app()->miniTrace();
+ *
+ */
+class MiniTrace extends CBehavior
+{
+    public function getMiniTrace(): string
+    {
+        $result = [];
+        $trace  = debug_backtrace();
+        array_shift($trace);
+        foreach ($trace as $one) {
+            if (!isset($one['file']))
+                $result [] = 'unknown';
+            else
+                $result [] = sprintf('%s -- %s:%d', $one['function'], $one['file'], $one["line"]);
+        }
+
+        return implode("\n", $result);
+    }
+}
